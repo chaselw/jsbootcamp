7e14c9d5f1f9c52951da366e7e51c0d95f37e407
Chase Williams <chaselw@gmail.com>
2019-01-12 01:39:37 -0700

    Continued basics

---
+++ basics/temp-convert.js
@@ -0,0 +1,9 @@
+//Local variable F
+let farenheit = 32
+
+//F to K
+let kelvin = (farenheit + 459.67) * 5 / 9
+console.log(kelvin)
+//F to C
+let celsius = (farenheit - 32) * 5 / 9
+console.log(celsius)
